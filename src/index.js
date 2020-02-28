45c2b9ed81fb48ca6500246fd5561b999f72a306
RafaelPSantos <rafaelps999@gmail.com>
2020-02-27 22:02:21 -0300

    add redis and started to create sessions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 require('dotenv').config();
 
 const port = process.env.PORT;
+const secrets = process.env.SESSION_SECRET;
+const redis_host = process.env.REDIS_HOST
+const redis_port = process.env.REDIS_PORT
 
 const express = require('express');
-const cors = require('cors')
+const session = require('express-session');
+const redis = require('redis');
+const redisStore = require('connect-redis')(session);
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
+app.use(bodyParser.json());
+app.use(session({
+    secret: secrets,
+    store: new redisStore({
+      client: redis.createClient({ host: redis_host, port: redis_port })
+    }),
+    saveUninitialized: false,
+    resave: false
+}));
 app.use(require('./routes'));
 
 app.listen(port, () => console.log(`Service up on port: ${port}`));
--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,34 @@ const express = require('express');
 const router = express.Router();
 // const repository = require('./repository')
 
+function authorizedRequest(req, res, next){
+  let sess = req.session;
+  console.log(req);
+  let require_sign_in = ['/sign-in', 'health'].indexOf(req.path) == -1;
+  let didn_not_signed = sess.email == null;
+
+  if (require_sign_in && didn_not_signed) {
+    console.log('Unauthorized');
+    return res.status(401).end();
+  };
+
+  console.log('Authorized');
+  next();
+}
+
+// router.use(authorizedRequest);
+
 router.get('/health', (req, res) => {
   return res.status(200).send("I'm fine, thanks for asking!");
 });
 
 router.post('/sign-in', (req, res) => {
+  req.session.email = req.body.email;
   res.status(200).end('done');
 });
 
 router.post('/sign-out', (req, res) => {
+  req.session.destroy();
   res.status(200).end('done');
 });
 
@@ -19,7 +38,6 @@ router.post('/punch', (req, res) => {
 });
 
 router.get('/punches', (req, res) => {
-  console.log('request');
   res.status(200).send(
     [
       {
